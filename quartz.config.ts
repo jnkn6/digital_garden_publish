6da9ee7f3160bdc3a8dea588e5a116f6f7429f96
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2025-06-18 01:18:58 +0900

    update: Update urls and title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "J.J's digital garden",
+    pageTitle: "Digital garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: process.env.BASE_URL,
     ignorePatterns: ["private", "templates", ".obsidian", "_template", "_writing", ".vscode", ".gitignore"],
     defaultDateType: "modified",
     theme: {
